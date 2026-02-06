249ee117e519ba7a66f30e93840e62e9876b466b
Luiz Felipe <luiz.rya@gmail.com>
2026-02-05 21:53:43 -0300

    feat: validate price

--- src/main/java/com/api/hexagonal_architecture/adapter/output/persistence/entity/ProductEntity.java
+++ src/main/java/com/api/hexagonal_architecture/adapter/output/persistence/entity/ProductEntity.java
@@ -22,6 +22,7 @@ public class ProductEntity extends Product {
   }
 
   public ProductEntity(Long id, String name, BigDecimal price) {
+    validatePrice(price);
     this.id = id;
     this.name = name;
     this.price = price;
@@ -29,6 +30,14 @@ public class ProductEntity extends Product {
 
   public Long getId() {
     return id;
+  }
+
+  private void validatePrice(BigDecimal price) {
+    if (price == null)
+      throw new IllegalArgumentException("Price is required");
+
+    if (price.compareTo(BigDecimal.ZERO) <= 0)
+      throw new IllegalArgumentException("Price must be greater than zero");
   }
 
   public void setId(Long id) {
