4d57c373ae7c4e7c24e6d846f81a816745d4ad54
Luiz Felipe <luiz.rya@gmail.com>
2026-02-05 21:59:50 -0300

    ci: change test

--- src/test/java/com/api/hexagonal_architecture/HexagonalArchitectureApplicationTests.java
+++ src/test/java/com/api/hexagonal_architecture/HexagonalArchitectureApplicationTests.java
@@ -2,8 +2,10 @@ package com.api.hexagonal_architecture;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HexagonalArchitectureApplicationTests {
 
 	@Test
